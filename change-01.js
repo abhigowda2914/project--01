96942977447ce66298b2ce6b53de14dfd7d77452
abhigowda2914 <abhiramgowda2914@gmail.com>
2024-02-28 11:04:15 +0530

    changes-03

--- change-01.js
+++ change-01.js
@@ -1,3 +1,23 @@
+const refheading = document.getElementById("main-heading");
+
+refheading.textContent = "Fruit World";
+
+refheading.style.color = "orange";
+
+const res = document.getElementById("header");
+
+res.style.backgroundColor = "green";
+
+res.style.borderBottom = "3px solid orange";
+
+const ans =document.getElementById("basket-heading");
+
+ans.style.color = "green";
+
+const final = document.getElementById("thanks");
+final.innerHTML = "<p>Please visit us again</p>"
+
+
 const ans = document.getElementsByClassName("fruit");
 ans[2].style.backgroundColor = "yellow";
 for(let i = 0;i<ans.length;i++){
