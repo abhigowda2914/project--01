adc02abba371e56a72f7e2b63882a6a286f98689
abhigowda2914 <abhiramgowda2914@gmail.com>
2024-02-28 14:02:36 +0530

    query -01

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const refheading = document.getElementById("main-heading");
-
-refheading.textContent = "Fruit World";
-
-refheading.style.color = "orange";
-
-const res = document.getElementById("header");
-
-res.style.backgroundColor = "green";
-
-res.style.borderBottom = "3px solid orange";
-
-const ans =document.getElementById("basket-heading");
-
-ans.style.color = "green";
-
-const val = document.getElementsByClassName("fruit");
-val[2].style.backgroundColor = "yellow";
-for(let i = 0;i<val.length;i++){
-  val[i].style.fontWeight = "bold";
-}
 
 
 const final = document.getElementById("thanks");
@@ -30,3 +9,40 @@ listItem[4].style.color = "blue";
 for(let i = 0;i<listItem.length;i++){
   listItem[i].style.fontStyle = "italic";
 }
+
+
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.style.textAlign = "right";
+
+const headfruits = document.querySelector(".fruits");
+
+headfruits.style.backgroundColor = "grey";
+headfruits.style.padding = "30px";
+headfruits.style.margin = "30px";
+headfruits.style.width = "50%";
+headfruits.style.borderRadius = "5px";
+ headfruits.style.listStyleType = "none";
+
+const basketHeading = document.querySelector("h2");
+basketHeading.style.marginLeft = "30px";
+basketHeading.style.color = "brown";
+
+const fruititem = document.querySelectorAll('.fruit');
+
+for(let i = 0;i<fruititem.length;i++){
+  fruititem[i].style.backgroundColor="white";
+  fruititem[i].style.padding = "10px";
+  fruititem[i].style.borderRadius = "5x";
+  fruititem[i].style.margin = "10px";
+}
+
+const counteven = document.querySelectorAll('.fruit:nth-child(even)');
+
+for(let i = 0;i<counteven.length;i++){
+  counteven[i].style.backgroundColor = "red";
+  counteven[i].style.color ="white";
+}
+
+  
+
+
