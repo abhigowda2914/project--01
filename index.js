668c50182b7649d679b8f2f759af6e168ecd9040
abhigowda2914 <abhiramgowda2914@gmail.com>
2024-02-28 10:35:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const refheading = document.getElementById("main-heading");
+
+refheading.textContent = "Fruit World";
+
+refheading.style.color = "orange";
+
+const res = document.getElementById("header");
+
+res.style.backgroundColor = "green";
+
+res.style.borderBottom = "3px solid orange";
+
+const ans =document.getElementById("basket-heading");
+
+ans.style.color = "green";
+
+
+
+const final = document.getElementById("thanks");
+final.innerHTML = "<p>Please visit us again</p>"
