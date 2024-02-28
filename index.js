306dd7ada45cd621a55e1c4206c37f8619359aef
abhigowda2914 <abhiramgowda2914@gmail.com>
2024-02-28 10:41:26 +0530

    second change

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const ans =document.getElementById("basket-heading");
 
 ans.style.color = "green";
 
+const val = document.getElementsByClassName("fruit");
+val[2].style.backgroundColor = "yellow";
+for(let i = 0;i<val.length;i++){
+  val[i].style.fontWeight = "bold";
+}
 
 
 const final = document.getElementById("thanks");
